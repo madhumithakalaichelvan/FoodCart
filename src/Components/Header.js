96e117cb651eb72b6c504837ae0c0b3909c4db03
madhumithakalaichelvan <madhumithakalaichelvan@gmail.com>
2024-03-27 01:20:15 +0530

    Icons Added

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom";
 import './Header.css'
 import { useSelector } from "react-redux";
+import settingIcon from "../Assets/setting.png"
+import cartImage from "../Assets/carticons.png"
 
 
 export default function Header (){
@@ -14,8 +16,8 @@ export default function Header (){
          <nav>
                 <ul>
                     <li id="home" ><Link  to={'/home'} > Home </Link></li>
-                    <li id="setting" ><Link >Settings</Link></li>
-                    <li id="cart" ><Link to={'/cart'} >Cart {totalCount > 0 && <span className="notification-badge">{totalCount}</span>}</Link></li>
+                    <li id="setting" ><Link ><img src={settingIcon} width= "25px" height= "25px"></img></Link></li>
+                    <li id="cart" ><Link to={'/cart'} ><img src={cartImage} width= "25px" height= "25px"></img>{totalCount > 0 && <span className="notification-badge">{totalCount}</span>}</Link></li>
                 </ul>
             </nav>
     </>)
